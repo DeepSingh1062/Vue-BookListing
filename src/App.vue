d86b618f9b964e1d6fcc53ab1ed14465bbe0f977
DeepSingh1062 <46215658+DeepSingh1062@users.noreply.github.com>
2021-01-16 15:46:48 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,15 @@
   <div id="app">
     <img src="./assets/logo.png">
     <book-list></book-list>
-    <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
 import BookList from './components/BookList';
-import BookForm from './components/BookForm';
 export default {
   name: 'app',
   components: {
-    BookList,
-    BookForm
+    BookList
   },
 };
 </script>
