793f3cab7a1007402e05b1fdd75b963e74a5f444
DeepSingh1062 <46215658+DeepSingh1062@users.noreply.github.com>
2021-01-16 15:06:26 +0530

    Add files via upload

---
+++ src/components/BookForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <form class=""  v-on:submit.prevent = "bookSubmit(bookTitle,bookAuthor)">
+    <input type="text" name="title" value="" v-model="bookTitle" placeholder="Book Title">
+    <input type="text" name="author" value="" v-model="bookAuthor" placeholder="Book Author">
+    <button type="submit" name="button">Add Book</button>
+</form>
+</template>
+
+<script>
+export default {
+    name:"BookForm",
+    props:['books'],
+    data:()=>{
+        return(
+            {
+                bookTitle:"",
+                bookAuthor:""
+            }
+        )
+    },
+    methods:{
+        bookSubmit:function(bookTitle,bookAuthor){
+            console.log(" i am here",bookTitle);
+            this.$emit('addBook', bookTitle, bookAuthor);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/BookList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+        <ul>
+            <book-item v-for="book in books" :book="book"></book-item>
+        </ul>
+    </div>
+</template>
+
+<script>
+import BookItem from "./BookItem";
+export default {
+    name:"BookList",
+    components:{
+        BookItem
+    },
+    methods:{
+        appendBook:(bookTitle,bookAuthor)=>{
+            this.books.push({ title: bookTitle, author: bookAuthor })
+        }
+    },
+    data:()=>{
+        return (
+            {
+    title:"All Books",
+    books:[
+    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+    {title: 'American Gods', author: 'Neil Gaiman'},
+    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+]
+            }
+        )
+    }
+}
+</script>
+
+<style scoped>
+h1,h2{
+    font-weight: normal;
+}
+
+ul{
+    list-style-type: none;
+    padding: 0;
+}
+</style>
